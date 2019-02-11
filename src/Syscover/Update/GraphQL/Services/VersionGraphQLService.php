8d230f6b4aef259251c6bbb8f3de009750a6389d
Carlos Rodríguez Palacín <cpalacin@digitalh2.com>
2019-02-11 20:45:56 +0100

    add graphQL schema

---
+++ src/Syscover/Update/GraphQL/Services/VersionGraphQLService.php
@@ -0,0 +1,14 @@
+<?php namespace Syscover\Admin\GraphQL\Services;
+
+use Syscover\Update\Models\Version;
+use Syscover\Update\Services\VersionService;
+use Syscover\Core\GraphQL\Services\CoreGraphQLService;
+
+class VersionGraphQLService extends CoreGraphQLService
+{
+    public function __construct(Version $model, VersionService $service)
+    {
+        $this->model = $model;
+        $this->service = $service;
+    }
+}
